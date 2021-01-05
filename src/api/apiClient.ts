537d4229f3ea086d8e69a528cea2cb752eccef0f
Jhon Peter <jp.pacinos@gmail.com>
2021-01-05 21:49:39 +0800

    update api

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,14 +1,18 @@
 import axios from 'axios'
 
-// const baseURL = 'http://127.0.0.1:8000/api'
-const baseURL = '/api'
+const baseURL = 'http://127.0.0.1:8000/api' // separate
+// const baseURL = '/api' // in server
 
 const apiClient = axios.create({
   baseURL: baseURL,
+  headers: {
+    Accept: 'application/json',
+  },
 })
 
 const authToken = sessionStorage.getItem('auth-token')
-
-apiClient.defaults.headers.common['Authorization'] = `Bearer ${authToken}`
+if (authToken) {
+  apiClient.defaults.headers.common['Authorization'] = `Bearer ${authToken}`
+}
 
 export default apiClient
